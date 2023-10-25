362513826833c1cd44eb585bb2a0ba2155f8fb42
Alex Estevez <aestevez@college.harvard.edu>
2023-10-25 19:37:26 -0400

    Organizational setup and dependency installation

---
+++ src/containers/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { HighlightText } from './styles'
+
+const Main = () => {
+    return (
+        <div>
+            finally some fucking web dev
+            <HighlightText>this is here as an example of styled components</ HighlightText>
+        </div>
+    )
+}
+
+export default Main
---
+++ src/containers/Main/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const HighlightText = styled.p`
+    color: #FF0000;
+    font-size: 24px;
+`
